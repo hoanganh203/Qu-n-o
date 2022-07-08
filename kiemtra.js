4cf04379dd3493e0ec94b6a801a7a6bdea4c52f9
hoanganh203 <105597730+hoanganh203@users.noreply.github.com>
2022-07-09 05:21:45 +0700

    Add new project Bán hàng

---
+++ kiemtra.js
@@ -0,0 +1,43 @@
+function kiemtra(e){
+    event.preventDefault();
+     var usename = document.getElementById("usename").value;
+     var password = document.getElementById("password").value;
+
+     var use = {
+        usename : usename,
+        password : password,
+     };
+
+     var josn=JSON.stringify(use);
+     localStorage.setItem(usename,josn);
+     alert("Đăng ký thành công")
+     window.location="login.html"
+
+}
+
+function login(e){
+    event.preventDefault();
+    var usename = document.getElementById("usename").value;
+    var password = document.getElementById("password").value;
+    var use = localStorage.getItem(usename);
+    var dulieu = JSON.parse(use);
+
+    if(dulieu.usename=="" && dulieu.password==""){
+        alert("Vui lòng nhập UseName and Password")
+        document.getElementById("usename").style.background='yellow'
+        document.getElementById("password").style.background='yellow'
+
+    }
+
+    else if(usename == dulieu.usename && password == dulieu.password)
+    {
+     alert("Đăng nhập thành công")
+    window.open("http://127.0.0.1:5500/index.html")
+    }
+
+    else{
+        alert("Sai useName of Password");
+        document.getElementById("usename").style.background='yellow'
+        document.getElementById("password").style.background='yellow'
+    }
+}
